618e634f21fd6be909b5faab81ffce7c5d2a2597
liammc0627 <liamccbel@outlook.com>
2025-11-06 15:55:12 +0100

    Delete practicaArrayLiam directory

--- practicaArrayLiam/Ejercicio1.js
+++
@@ -1,9 +0,0 @@
-for (let j = 0; j < 50; j++) {
-  let numeros = [];
-
-  while (numeros.length < 6) {
-    let numero = Math.floor(Math.random() * 49) + 1;
-      numeros.push(numero);
-  }
-  console.log(numeros.join(' '));
-}
